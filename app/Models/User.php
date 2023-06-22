9f84a08511dd659d6ce6ec9171a9a92fcacd5390
mkbcentral <mkbcentralcode@gmail.com>
2023-06-22 12:56:39 +0200

    Make filament home url 5

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\RoleEnum;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -12,7 +13,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
 
@@ -65,5 +66,10 @@ class User extends Authenticatable
     public function patient(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@mkbcentral.com') && $this->hasVerifiedEmail();
     }
 }
