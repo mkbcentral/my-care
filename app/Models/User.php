d70af029a10e65a69cfac045a7c82f0afb2176fc
mkbcentral <mkbcentralcode@gmail.com>
2023-06-22 13:00:44 +0200

    Make filament home url 6

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,6 +70,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@my-care.mkbcentral.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
 }
